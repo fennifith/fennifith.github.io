34e5376b2d1600fe6148d773b8afec38dfc6d3be
James Fenn <18jafenn90@gmail.com>
2015-06-24 13:38:21 -0400

    Update scriptschat.js

--- scriptschat.js
+++ scriptschat.js
@@ -1,5 +1,7 @@
 var app = angular.module('Todo', []);
 app.controller('TodoCtrl', function($scope) {
+  $scope.newTodo = '';
+
   $scope.todos = [
     'Learn Sketch', 
     'Look at Dribbble and feel inferior',
@@ -10,6 +12,13 @@ app.controller('TodoCtrl', function($scope) {
     var indexOf = $scope.todos.indexOf(todo);
     if (indexOf !== -1) {
       $scope.todos.splice(indexOf, 1);
+    }
+  };
+  
+   $scope.add = function(e) {
+    if (e.which && e.which === 13) {
+      $scope.todos.push($scope.newTodo);
+      $scope.newTodo = '';
     }
   };
 });
