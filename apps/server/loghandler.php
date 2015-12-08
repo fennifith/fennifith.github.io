24282e95c5b32150f8c847acf4bb3bb0bfa3904e
James Fenn <18jafenn90@gmail.com>
2015-12-07 21:02:57 -0500

    Update loghandler.php

--- apps/server/loghandler.php
+++ apps/server/loghandler.php
@@ -6,20 +6,41 @@
 header("Access-Control-Allow-Origin: http://theandroidmaster.github.io");
 
 $filename = $_GET["fchat"];
+
 if (empty($filename)) {
  $filename = "log";
 }
 
-if (empty($_SESSION[$filename])) {
- session_start();
+$filename = $filename . ".txt";
+$myoldfile = fopen($filename, "r");
+
+if (empty($myoldfile)) {
+ fclose($myoldfile);
+ $myfile = fopen($filename, w);
+ while(!$myfile) {
+  $myfile = fopen($filename, w);
+ }
+ fclose($myfile);
+ $myoldfile = fopen($filename, "r");
 }
 
 $contents = fread($myoldfile, filesize($filename));
+fclose($myoldfile);
+
 $myfile = fopen($filename, "w");
+while(!$myfile) {
+ $myfile = fopen($filename, "w");
+}
+
 if (!empty($_GET["fcontent"])) {
- $txt = $_GET["fname"] . ": " . $_GET["fcontent"] . "<br>" . $_SESSION[$filename];
+ $txt = $_GET["fname"] . ": " . $_GET["fcontent"] . "<br>" . $contents;
  if (strlen($txt) > 1000) $txt = substr($txt, 0, 1000) . "<br>...";
- $_SESSION[$filename] = $txt;
+ fwrite($myfile, $txt);
+}
+fclose($myfile);
+
+if ($_GET["fname"] == "delete") {
+ unlink($filename);
 }
 
 echo $txt;
