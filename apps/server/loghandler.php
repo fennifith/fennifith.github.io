a6b7a195b2176af871ab1d89dd4d3e326a7b74a1
James Fenn <18jafenn90@gmail.com>
2015-12-11 16:19:42 -0500

    Update loghandler.php

--- apps/server/loghandler.php
+++ apps/server/loghandler.php
@@ -53,27 +53,54 @@ if (empty($fcontent)) {
   
   //colors
   if (strpos($fcontent, "/red ") !== false) {
-   $fcontent = "<p style=\"color:red;\">" . str_replace("/red ", "", $fcontent) . "</p>";
+   $fcontent = "<p style=\"color:#f44336;\">" . str_replace("/red ", "", $fcontent) . "</p>";
   }
-  if (strpos($fcontent, "/orange ") !== false) {
-   $fcontent = "<p style=\"color:orange;\">" . str_replace("/orange ", "", $fcontent) . "</p>";
-  }
-  if (strpos($fcontent, "/yellow ") !== false) {
-   $fcontent = "<p style=\"color:yellow;\">" . str_replace("/yellow ", "", $fcontent) . "</p>";
-  }
-  if (strpos($fcontent, "/green ") !== false) {
-   $fcontent = "<p style=\"color:green;\">" . str_replace("/green ", "", $fcontent) . "</p>";
-  }
-  if (strpos($fcontent, "/blue ") !== false) {
-   $fcontent = "<p style=\"color:blue;\">" . str_replace("/blue ", "", $fcontent) . "</p>";
+  if (strpos($fcontent, "/pink ") !== false) {
+   $fcontent = "<p style=\"color:#e91e63;\">" . str_replace("/pink ", "", $fcontent) . "</p>";
   }
   if (strpos($fcontent, "/purple ") !== false) {
-   $fcontent = "<p style=\"color:purple;\">" . str_replace("/purple ", "", $fcontent) . "</p>";
+   $fcontent = "<p style=\"color:#9c27B0;\">" . str_replace("/purple ", "", $fcontent) . "</p>";
+  }
+  if (strpos($fcontent, "/indigo ") !== false) {
+   $fcontent = "<p style=\"color:#3f51b5;\">" . str_replace("/indigo ", "", $fcontent) . "</p>";
+  }
+  if (strpos($fcontent, "/blue ") !== false) {
+   $fcontent = "<p style=\"color:#2196f3;\">" . str_replace("/blue ", "", $fcontent) . "</p>";
+  }
+  if (strpos($fcontent, "/cyan ") !== false) {
+   $fcontent = "<p style=\"color:#0097a7;\">" . str_replace("/cyan ", "", $fcontent) . "</p>";
+  }
+  if (strpos($fcontent, "/teal ") !== false) {
+   $fcontent = "<p style=\"color:#009688;\">" . str_replace("/cyan ", "", $fcontent) . "</p>";
+  }
+  if (strpos($fcontent, "/green ") !== false) {
+   $fcontent = "<p style=\"color:#43a047;\">" . str_replace("/green ", "", $fcontent) . "</p>";
+  }
+  if (strpos($fcontent, "/lime ") !== false) {
+   $fcontent = "<p style=\"color:#827717;\">" . str_replace("/lime ", "", $fcontent) . "</p>";
+  }
+  if (strpos($fcontent, "/yellow ") !== false) {
+   $fcontent = "<p style=\"color:#FBC02D;\">" . str_replace("/yellow ", "", $fcontent) . "</p>";
+  }
+  if (strpos($fcontent, "/orange ") !== false) {
+   $fcontent = "<p style=\"color:#ef6c00;\">" . str_replace("/orange ", "", $fcontent) . "</p>";
+  }
+  if (strpos($fcontent, "/brown ") !== false) {
+   $fcontent = "<p style=\"color:#795548;\">" . str_replace("/brown ", "", $fcontent) . "</p>";
   }
   
   //images
   if (strpos($fcontent, "/img ") !== false) {
    $fcontent = "<img src=\"" . str_replace("/img ", "", $fcontent) . "\"></img>";
+  }
+  
+  //links
+  if (strpos($fcontent, "/link " ) !== false) {
+   $fcontent = str_replace("/link ", "", $fcontent);
+   $str = $fcontent;
+   $str = trim(preg_replace('/\s+/', ' ', $str));
+   preg_match("/\<title\>(.*)\<\/title\>/i", $str, $title);
+   $fcontent = "<a href=\"" . $fcontent ."\">" . $title[1] . "</a>";
   }
   
   $txt = $fname . " - " . date('Y-m-d H:i:s') . ": " . $fcontent . "<br>" . $contents;
