267cf97da5fffcf2f1e0791f0f2e16333ab5a919
James Fenn <18jafenn90@gmail.com>
2018-08-18 14:43:21 -0400

    added commit functionality to update script

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,3 +1,5 @@
+git pull
+
 jekyll serve &
 JEKYLL_ID=$!
 
@@ -7,4 +9,10 @@ rm -rf node_modules && npm install
 node update.js
 
 kill -9 "$JEKYLL_ID"
+
+read -p "[Enter] to commit & push, [Ctrl+C] to cancel."
+git add ../../_projects
+git commit -m "Auto-commit: updated project files"
+git push
+
 exit 0
