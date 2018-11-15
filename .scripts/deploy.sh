dff0b3451c1c79cc77b96942dbf9a0ff602c9757
James Fenn <18jafenn90@gmail.com>
2018-11-15 14:34:23 -0500

    aaaaaaaa debugging

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -7,7 +7,7 @@ then
 	git config --global user.email "travis@travis-ci.com"
 
 	git commit -m "Auto-commit (Travis) $TRAVIS_BUILD_NUMBER"
-	git push https://fennifith:${GITHUB_TOKEN}@github.com/fennifith/fennifith.github.io HEAD:master --quiet > /dev/null 2>&1
+	git push https://fennifith:${GITHUB_TOKEN}@github.com/fennifith/fennifith.github.io HEAD:master
 fi
 
 exit 0
