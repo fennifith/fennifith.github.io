24e0e126084a785e8dc1631548138bd18ea9f696
James Fenn <18jafenn90@gmail.com>
2018-11-15 14:54:17 -0500

    trying everything at once

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -7,8 +7,10 @@ then
 	git config --global user.email "travis@travis-ci.com"
 
 	git commit -m "Auto-commit (Travis) $TRAVIS_BUILD_NUMBER"
+	git remote set-url origin https://fennifith:${GITHUB_TOKEN}@github.com/fennifith/fennifith.github.io.git/
 	git fetch --unshallow || true
-	git push https://fennifith:${GITHUB_TOKEN}@github.com/fennifith/fennifith.github.io HEAD:master
+	git checkout master
+	git push origin master
 fi
 
 exit 0
