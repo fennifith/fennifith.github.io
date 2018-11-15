166513b9fc8bfeea38efd70ee63303a740c38c23
James Fenn <18jafenn90@gmail.com>
2018-11-15 15:01:39 -0500

    change add/checkout/aaaaaaaaaa order

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -6,10 +6,11 @@ then
 	git config --global user.name "Travis CI"
 	git config --global user.email "travis@travis-ci.com"
 
-	git commit -m "Auto-commit (Travis) $TRAVIS_BUILD_NUMBER"
-	git remote set-url origin https://fennifith:${GITHUB_TOKEN}@github.com/fennifith/fennifith.github.io.git/
 	git fetch --unshallow || true
 	git checkout master
+	git add projects _projects _people _orgs
+	git commit -m "Auto-commit (Travis) $TRAVIS_BUILD_NUMBER"
+	git remote set-url origin https://fennifith:${GITHUB_TOKEN}@github.com/fennifith/fennifith.github.io.git/
 	git push origin master
 fi
 
