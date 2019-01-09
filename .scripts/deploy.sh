d5a17495e95c55c8e70da287dc420e084f2c493b
James Fenn <18jafenn90@gmail.com>
2019-01-08 19:57:30 -0500

    skip travis deployment commits as well

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -9,7 +9,7 @@ then
 	git fetch --unshallow || true
 	git checkout master
 	git add projects _projects _people _orgs
-	git commit -m "Auto-commit (Travis) $TRAVIS_BUILD_NUMBER"
+	git commit -m "Auto-commit (Travis) $TRAVIS_BUILD_NUMBER [ci skip]"
 	git remote set-url origin https://fennifith:${GITHUB_TOKEN}@github.com/fennifith/fennifith.github.io.git/
 	git push origin master
 fi
