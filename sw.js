09cc10fbd00b92402127b05c01a63a4bd84c6858
James Fenn <me@jfenn.me>
2024-02-10 21:00:29 -0500

    deploy

---
+++ sw.js
@@ -0,0 +1,55 @@
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.1/workbox-sw.js');
+
+const _version = '-1';
+
+if (workbox) {
+    workbox.precaching.precacheAndRoute([]);
+
+    // Cache the Google Fonts stylesheets with a stale while revalidate strategy.
+    workbox.routing.registerRoute(
+        /^https:\/\/fonts\.googleapis\.com/,
+        workbox.strategies.staleWhileRevalidate({
+            cacheName: 'google-fonts-stylesheets',
+        }),
+    );
+
+    // Cache the Google Fonts webfont files with a cache first strategy for 1 year.
+    workbox.routing.registerRoute(
+        /^https:\/\/fonts\.gstatic\.com/,
+        workbox.strategies.cacheFirst({
+            cacheName: 'google-fonts-webfonts',
+            plugins: [
+                new workbox.cacheableResponse.Plugin({
+                    statuses: [0, 200],
+                }),
+            	new workbox.expiration.Plugin({
+                	maxAgeSeconds: 60 * 60 * 24 * 365,
+            	}),
+        	],
+    	}),
+	);
+
+    workbox.routing.registerRoute(
+        // Cache CSS files
+        /.*\.css/,
+        workbox.strategies.staleWhileRevalidate({
+            cacheName: 'css-cache' + _version,
+        })
+    );
+
+    workbox.routing.registerRoute(
+        // Cache image files
+        /.*\.(?:png|jpg|jpeg|svg|gif)/,
+        workbox.strategies.cacheFirst({
+            cacheName: 'image-cache' + _version,
+            plugins: [
+                new workbox.expiration.Plugin({
+                    maxEntries: 100,
+                    maxAgeSeconds: 7 * 24 * 60 * 60,
+                })
+            ],
+        })
+    );
+} else {
+    console.log("Workbox didn't load...");
+}
